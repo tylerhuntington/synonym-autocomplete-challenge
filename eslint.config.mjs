2ce6adff247dedf053cdc97375bf29c9569b4bcb
Tyler Huntington <tylerhuntington222@lbl.gov>
2025-02-28 22:17:54 -0800

    Update eslint.config.mjs to loosen linting contraints for deployment on Render.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unused-vars': 'off',
+    },
+  }),
+]
 export default eslintConfig;
